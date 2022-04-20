1c843e318e7096d2e8ea9b64a1adf7bc0cd28f09
Marc Anton Dahmen <revitron@marcdahmen.de>
2022-04-20 13:46:32 +0200

    add favicon

--- public/index.php
+++ public/index.php
@@ -16,6 +16,7 @@ new Environment()
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Revitron Charts</title>
+	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
 	<link rel="stylesheet" href="dist/vendor.bundle.css">
 	<link rel="stylesheet" href="dist/main.bundle.css">
 	<script src="dist/vendor.bundle.js"></script>
